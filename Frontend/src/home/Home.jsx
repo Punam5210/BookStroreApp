90587bb23180f98cbc2d31fef3385be665b637f3
Punam Kumari <punamkumari.riverside@gmail.com>
2024-10-23 20:40:00 +0530

    Backend Done

---
+++ Frontend/src/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../component/navbar";
+import Banner from "../component/Banner";
+import Footer from "../component/Footer";
+import Freebook from "../component/Freebook";
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <Freebook/>
+    <Footer/>
+    
+    
+    </>
+  );
+}
+
+export default Home;
